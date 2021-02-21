13ce8cca876de1c42ce9f5041ae7e829a4138131
Micko Rada <mr6517430@gmail.com>
2021-02-21 11:24:09 -0800

    assignments for the main function

--- assignments.py
+++ assignments.py
@@ -1,7 +1,3 @@
-from tkinter import *
-from timeit import default_timer as timer 
-import random 
-
 window = Tk()
 window.geometry("450x200")
 Title=Label(window,text="lets start this game ...", font="times 20")
@@ -13,17 +9,5 @@ def speedtest_game():
 b1=Button(window,text="goo",command=speedtest_game,width=12,bg='gray')
 b1.place(x=150,y=100)
 
-
 Title.pack()
 window.mainloop()
-'''
-windows.geometry("450x200")
-x1=label(window,text="lets start this game ...", font="times 20")
-x1.place(x=10, y=50)
-
-def game():
-    print("")
-b1=Button(window,text="goo",command=game,width=12,bg='gray')
-b1.place(x=150,y=100)
-
-window.mainloop()'''
