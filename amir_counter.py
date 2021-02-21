e994b500556d6670e4494e16438fe3f0509ef8ff
Amir <amir@sysarchitect.local>
2021-02-21 12:15:02 -0800

    in-function variables changed, comments added

--- amir_counter.py
+++ amir_counter.py
@@ -1,7 +1,9 @@
 from Tkinter import *
 
-root = Tk()
-root.counter = 0
+
+#Creating the tk window
+window = Tk()
+window.counter = 0
 
 #This function increases the counter by 1 everytime the button is clicked.
 def counter_click():
