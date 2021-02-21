1e60d9f6f4209a122d5c0db2b3171f70b192cd28
Amir <amir@sysarchitect.local>
2021-02-21 12:11:28 -0800

    final version, both functions added

--- amir_counter.py
+++ amir_counter.py
@@ -5,13 +5,13 @@ root.counter = 0
 
 #This function increases the counter by 1 everytime the button is clicked.
 def counter_click():
-    root.counter += 1
-    L['text'] = 'Button clicked: ' + str(root.counter)
+    window.counter += 1
+    Title['text'] = 'Button clicked: ' + str(window.counter)
         
 
 #This function kills the window, when the button is pressed.
 def counter_kill():
-	root.destroy()
+	window.destroy()
 
 
 root.mainloop()
