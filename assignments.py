0658d8abde0ae07c0afe45f5f2d3dd1709b29f68
Micko Rada <mr6517430@gmail.com>
2021-02-21 12:28:00 -0800

    created the assignment and import files

--- assignments.py
+++ assignments.py
@@ -1,13 +1,18 @@
+#Creating the tk window
 window = Tk()
 window.geometry("450x200")
-Title=Label(window,text="lets start this game ...", font="times 20")
+Title=Label(window,text="lets start the counter!", font="times 20")
 Title.place(x=10,y=50)
+window.counter = 0
 
-def counter_click():
-    print(" ")
+#Creates the button for the counter
+b1=Button(window,text="Click me ",command=counter_click,width=12,bg='gray')
+b1.place(x=150,y=100)
 
-b=Button(window,text="Click me ",command=counter_click,width=12,bg='gray')
-b.place(x=150,y=100)
+#Creates the button to end the counter
+b2=Button(window,text="End Counter",command=counter_kill,bg='red')
+b2.place(x=150,y=130)
 
+#Pushes gui to Desktop
 Title.pack()
 window.mainloop()
